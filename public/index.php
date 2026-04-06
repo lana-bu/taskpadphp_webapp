b9a74319a6ecf6e379db7c84314b1ab63aa40989
lana-bu <lanabu@umich.edu>
2026-04-06 16:10:18 -0400

    logo and styles setup

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
 <!-- list and filter (GET), flash messages -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Twinkling Meadows Brochure</title>
+    <meta name="description" content="Get information on the Twinkling Meadows stargazing destination and select between travel package options.">
+    <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+<body>
+    <header class="header-container">
+        <h1 class="site-name site-name-large">TaskPadPHP</h1>
+        <img class="logo" src="./assets/images/logo.png"/>
+    </header>
+    <main class="content-container">
+        
+    </main>
+    <footer class="footer-container">
+        <h3 class="site-name">TaskPadPHP</h3>
+    </footer>
+</body>
+</html>
