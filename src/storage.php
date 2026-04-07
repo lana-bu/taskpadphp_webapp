3c1c1e1a78c68caccda6bf69a0409c314e155a51
lana-bu <lanabu@umich.edu>
2026-04-07 13:18:55 -0400

    Storage class (loading and saving json file)

--- src/storage.php
+++ src/storage.php
@@ -1,1 +1,28 @@
 <!-- load/save JSON helpers -->
+<?php
+    class Storage {
+        private string $path;
+        private string $json;
+        private array $tasks;
+
+        public function __construct(string $path) {
+            $this->path = $path;
+            $this->json = file_get_contents($this->path);
+
+            if ($this->json === false) {
+                die('Error reading the JSON file');
+            }
+
+            $this->tasks = json_decode($this->json, true);
+        }
+
+        public function getTasks() : array {
+            return $this->tasks;
+        }
+
+        public function saveJson(array $tasks) {
+            $this->tasks = $tasks;
+            $this->json = json_encode($this->tasks);
+            file_put_contents($this->path, $json);
+        }
+    }
